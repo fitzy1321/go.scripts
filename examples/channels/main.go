03a5504cea49490da3de9d8c0e3a3db4e411e679
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2025-07-26 15:18:03 -0600

    cleanup channel example code

--- examples/channels/main.go
+++ examples/channels/main.go
@@ -3,40 +3,50 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 )
 
 func main() {
+	// create a channel. This should be json strings
 	c_json_str := make(chan string)
-	// resp, err := http.Get("https://google.com")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println()
-	go something(c_json_str)
+
+	// goroutine, call websites and parse stuff
+	go callWebsites(c_json_str)
+
+	// iterating over a channel, will wait for data then iterate
+	// channel must be closed, to prevent infinite deadlock after goroutine ends
 	for result := range c_json_str {
+		// print json string to screen
 		fmt.Println("Returned from call:", result)
-		fmt.Printf("Type of 'result' is %T\n", result)
 	}
-	// fmt.Println()
 }
 
-// const
-
+// An object to turn into a json string
 type Response struct {
 	SiteName   string `json:"site"`
 	StatusCode int    `json:"status"`
 	Message    string `json:"message"`
 }
 
-func something(c chan string) {
+func callWebsites(c chan string) {
+	// defer will call close once the function ends
 	defer close(c)
+
+	// simulating of hitting websites and getting a response
 	results := []Response{
 		{SiteName: "google.com", StatusCode: 200, Message: ""},
 		{SiteName: "amazon.com", StatusCode: 200, Message: "Ready to buy something?"},
 		{SiteName: "some_nonsense.com", StatusCode: 404, Message: "Site not found!"},
 	}
+
+	// turning objects into json strings
+	// errors witll be logged and parsed to error strings
 	for _, r := range results {
-		j, _ := json.Marshal(r)
+		j, err := json.Marshal(r)
+		if err != nil {
+			log.Printf("Error occured parsing objects to json str: %s", err)
+			c <- fmt.Sprintf(`{"error": "%s"}`, err)
+		}
 		c <- string(j)
 	}
 
